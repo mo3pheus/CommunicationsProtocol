c702f0f0c62bcc10d0344ec3bd74c2f0fd439859
Korgaonkar, Sanket <Sanket.Korgaonkar@searshc.com>
2018-01-12 15:18:39 -0600

    Feature: Added DAN Module.

--- src/main/java/communications/protocol/ModuleDirectory.java
+++ src/main/java/communications/protocol/ModuleDirectory.java
@@ -5,7 +5,8 @@ public class ModuleDirectory {
         SENSOR_LIDAR(0, "Sensor Lidar"), PROPULSION(1, "Propulsion Module"), COMS(2, "Communications Module"),
         DIAGNOSTICS(3, "Diagnostics Module"), POWER(4, "Power Unit"), SCIENCE(5, "Science Module"),
         KERNEL(6, "OS Kernel"), SENSOR_SPECTROMETER(7, "Spectrometer"), CAMERA_SENSOR(8, "Camera Module"),
-        RADAR(9, "Radar Unit"), WEATHER_SENSOR(10, "REMS"), SPACECRAFT_CLOCK(11, "Spacecraft Clock");
+        RADAR(9, "Radar Unit"), WEATHER_SENSOR(10, "REMS"), SPACECRAFT_CLOCK(11, "Spacecraft Clock"),
+        DAN_SPECTROMETER(12, "Dynamic Albedo Neutron Spectrometer");
 
         int value;
 
