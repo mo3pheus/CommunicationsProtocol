a8ea01c8b2de88bd324f229beec1ee8b61165f7a
Sanket <sanket.korgaonkar@gmail.com>
2018-08-03 00:40:40 -0500

    Feature: Initial SAM compatibility

--- src/main/java/communications/protocol/ModuleDirectory.java
+++ src/main/java/communications/protocol/ModuleDirectory.java
@@ -6,7 +6,7 @@ public class ModuleDirectory {
         DIAGNOSTICS(3, "Diagnostics Module"), POWER(4, "Power Unit"), SCIENCE(5, "Science Module"),
         KERNEL(6, "OS Kernel"), SENSOR_SPECTROMETER(7, "Spectrometer"), CAMERA_SENSOR(8, "Camera Module"),
         RADAR(9, "Radar Unit"), WEATHER_SENSOR(10, "REMS"), SPACECRAFT_CLOCK(11, "Spacecraft Clock"),
-        DAN_SPECTROMETER(12, "Dynamic Albedo Neutron Spectrometer");
+        DAN_SPECTROMETER(12, "Dynamic Albedo Neutron Spectrometer"), SAM_SPECTROMETER(13, "Sample Analysis on MSL");
 
         int value;
 
